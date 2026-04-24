65d9ce7cc8fa1fe7d9e9f993efdebb64e4a17156
lucassilva <l.515@hotmail.com>
2026-04-24 17:03:21 -0300

    Primeiro commit

---
+++ Java/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,30 @@
+package fundamentos;
+
+/**
+ * documentação
+ */
+
+public class PrimeiroPrograma {
+	/**
+	 * 
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		
+		//Sentença de codigo termina com ;
+		//Mais um comentario
+		//Fim
+		System.out.println("Primeiro programa Parte #01!");
+//		System.out.println("Primeiro programa Parte #02!");
+		
+		/*
+		 * 
+		 *Linha 1
+		 *Linha 2 
+		 *Linha 3
+		 *
+		 * */
+		
+	}
+}
