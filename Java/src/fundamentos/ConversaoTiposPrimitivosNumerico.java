114c341e0e419e871f9be1204f779089f3af4062
lucassilva <l.515@hotmail.com>
2026-04-28 17:31:09 -0300

    "Segundo Commit"

---
+++ Java/src/fundamentos/ConversaoTiposPrimitivosNumerico.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class ConversaoTiposPrimitivosNumerico {
+	public static void main(String[] args) {
+		
+		
+		double a = 1; // implicita
+		System.out.println(a);
+		
+		float b = (float) 1.123456788888; // explicita (CAST)
+		System.out.println(b);
+	}
+}
--- Java/src/fundamentos/Wrapper.java
+++ Java/src/fundamentos/Wrapper.java
@@ -15,5 +15,18 @@ public class Wrapper {
 		System.out.println(s.toString());
 		System.out.println(i * 3);
 		System.out.println(l / 3);
+
+		Float f = 123.10F;
+		System.out.println(f);
+		
+		double d = 1234.5678;
+		System.out.println(d);
+		
+		Boolean bo = Boolean.parseBoolean("true");
+		System.out.println(bo);
+		System.out.println(bo.toString().toUpperCase());
+		
+		Character c = '#'; // char
+		System.out.println(c + "...");
 	}
 }
